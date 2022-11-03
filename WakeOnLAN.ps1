4dfd2b619a3312fdcc4c16fef21e1e3bc33d2ef4
ingramcomputers <79795957+ingramcomputers@users.noreply.github.com>
2022-11-02 19:34:15 -0500

    Cred WOL Script

--- WakeOnLAN.ps1
+++ WakeOnLAN.ps1
@@ -1,5 +1,6 @@
 # Create a runtime variable named: Mac
 # When you run the script, enter the device's MAC address
+# Script is based from: https://www.pdq.com/blog/wake-on-lan-wol-magic-packet-powershell/
 
 $MacByteArray = $Mac -split "[:-]" | ForEach-Object { [Byte] "0x$_"}
 [Byte[]] $MagicPacket = (,0xFF * 6) + ($MacByteArray  * 16)
