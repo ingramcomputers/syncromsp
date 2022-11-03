f46c15e70ed1116c910e4504f0355ec2031ee56d
ingramcomputers <79795957+ingramcomputers@users.noreply.github.com>
2022-11-02 19:24:05 -0500

    New script

---
+++ WakeOnLAN.ps1
@@ -0,0 +1,9 @@
+# Create a runtime variable named: Mac
+# When you run the script, enter the device's MAC address
+
+$MacByteArray = $Mac -split "[:-]" | ForEach-Object { [Byte] "0x$_"}
+[Byte[]] $MagicPacket = (,0xFF * 6) + ($MacByteArray  * 16)
+$UdpClient = New-Object System.Net.Sockets.UdpClient
+$UdpClient.Connect(([System.Net.IPAddress]::Broadcast),7)
+$UdpClient.Send($MagicPacket,$MagicPacket.Length)
+$UdpClient.Close()
